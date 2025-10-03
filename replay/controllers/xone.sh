cc96e0fa8ed73ef24546df1fc4caa27cfe6afd2c
Kev <36553759+forkymcforkface@users.noreply.github.com>
2025-10-03 09:35:43 -0700

    updated
    added updated xone install
    added git pull vs delete folders and reclone

--- replay/controllers/xone.sh
+++ replay/controllers/xone.sh
@@ -9,29 +9,29 @@ sudo apt install -y -qq git dkms build-essential patch libasound2-dev usbutils
 
 cd /opt/xbox-drv
 
-echo ">> Cloning xone repo..."
-rm -rf xone
-git clone https://github.com/dlundqvist/xone
-cd xone
-
-echo ">> Uninstalling xone module..."
-modprobe -r xone_gip_headset xone_gip_gamepad xone_dongle xone_gip || true
-./uninstall.sh
+echo ">> Cloning/updating xone repo..."
+if [ -d xone ]; then
+    cd xone
+    git pull
+else
+    git clone https://github.com/dlundqvist/xone
+    cd xone
+fi
 
 echo ">> Installing xone module..."
-./install.sh --release
-
-echo ">> Installing firmware..."
-./install/firmware.sh --skip-disclaimer
+sudo make install
 
 echo ">> Done. Custom xone driver patched and installed."
 
-
 echo ">> Installing xpad-noone to allow xone to manage xbox one controllers"
-echo ">> Cloning xpad repo..."
+echo ">> Cloning/updating xpad repo..."
 
 sudo modprobe -r xpad xpad-noone || true
-sudo git clone https://github.com/forkymcforkface/xpad-noone.git /usr/src/xpad-noone-1.0
+if [ -d /usr/src/xpad-noone-1.0 ]; then
+    sudo git -C /usr/src/xpad-noone-1.0 pull
+else
+    sudo git clone https://github.com/forkymcforkface/xpad-noone.git /usr/src/xpad-noone-1.0
+fi
 sudo dkms install -m xpad-noone -v 1.0
 echo 'xpad-noone' | sudo tee /etc/modules-load.d/xpad-noone.conf
 sudo modprobe xpad-noone
